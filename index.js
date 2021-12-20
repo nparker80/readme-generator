1b05dbcf954e9c98e91faff8147b3d4ca53527be
Najla Parker <najla08@gmail.com>
2021-12-19 17:40:03 -0700

    finished functions to initialize app

--- index.js
+++ index.js
@@ -1,87 +1,119 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const util = require("util");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-function promptUser(){
-  return inquirer.prompt ([
-    {
-      type: "input",
-      name: "projectName",
-      message: "What is the name of your project?",
-    },
+const userQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Welcome to the README generator! Please provide your full name:",
+  },
+  
+  {
+    type: "input",
+    name: "gitHub",
+    message: "Enter your GutHub username:",
+  },
+  
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
 
-    {
-      type: "input",
-      name: "projectDescription",
-      message: "Please write a brief description of your project:",
-    },
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the title of your project?",
+  },
+
+  {
+    type: "input",
+    name: "projectDescription",
+    message: "Please write a brief description of your project:",
+  },
     
-    {
-      type: "input",
-      name: "installationInstructions",
-      message: "Describe any items you installed to create the application:",
-    },
+  {
+    type: "input",
+    name: "installationInstructions",
+    message: "What are the instructions for installation?:",
+  },
 
-    {
-      type: "input",
-      name: "projectUsage",
-      message: "What does your application do? (provide a description of how the user interacts with your application):",
-    },
+  {
+    type: "input",
+    name: "projectUsage",
+    message: "What are the instructions for usage?:",
+  },
+  
+  {
+    type: "input",
+    name: "projectContributing",
+    message: "How can other people contribute to your project?",
+  },
 
-    {
-      type: "list",
-      name: "projectLicense",
-      message: "Choose the license you assigned to this project:",
-      choices: [
-        "MIT",
-        "Mozilla",
-        "ISC",
-        "GNU",
-        "The Unlicense",
-        "Apache"
-      ]
-    },
+  {
+    type: "input",
+    name: "projectTestInstructions",
+    message: "Describe the tests written for your app and how to use them:",
+  },
 
-    {
-      type: "input",
-      name: "projectcContributors",
-      message: "Who contributed to this project?",
-    },
+  {
+    type: "confirm",
+    name: "confirmLicenses",
+    message: "Would you like to include a license for your project?:",
+    default: false
+  },
 
-    {
-      type: "input",
-      name: "projectTestInstructions",
-      message: "Are there test instructions for this project?",
-    },
+  {
+    type: "list",
+    name: "projectLicense",
+    message: "Which license would you like to include for your project?:",
+    choices: [
+      "MIT",
+      "Mozilla",
+      "The Unlicense",
+      "Apache"
+    ]
+  },
 
-    {
-      type: "input",
-      name: "projectIssues",
-      message: "Who can I contact if I encounter issues with the application?",
-    },
-
-    {
-      type: "input",
-      name: "username",
-      message: "What is your GitHub username?",
-    },
-
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email address?",
-    }
-  ]);
-}
+  {
+    type: "input",
+    name: "projectIssues",
+    message: "Where can I email questions about the application?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+  returnNewPromise((resolve, reject) => {
+    fs.writeFile("./utils/README.md", data, err => {
+      if (err) {
+        reject (err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: console.log("Navigate to the utils folder to see your generated README!")
+      });
+    })
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  return inquirer.prompt(userQuestions);
+}
 
 // Function call to initialize app
-init();
+init ()
+.then(userInput => {
+  return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+  return writeToFile(readmeInfo);
+})
+.catch (err => {
+  console.log(err);
+})
